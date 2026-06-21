cf56cb37f69cc5aa7d367587a328a4ecb447b5f0
VictorHernandez <creacionesk593@gmail.com>
2026-06-21 10:28:05 -0500

    Update supabase-client.js
    con credenciales

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,8 +1,8 @@
 // js/supabase-client.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = "https://TU_PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY";
+const SUPABASE_URL = "https://zauyayrcrywazucdvmia.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphdXlheXJjcnl3YXp1Y2R2bWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMzMyNDMsImV4cCI6MjA5NzYwOTI0M30.BS-QCXTm5WDsevO2evSZPndIRhIGBeaAuF1opDY_7a4";
 
 // Congelamos el cliente para evitar manipulaciones en caliente (OWASP Client-Side Tampering)
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
@@ -13,3 +13,5 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     }
 });
 Object.freeze(supabase);
+
+
